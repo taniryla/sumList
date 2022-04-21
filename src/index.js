466ce1df3f92b7fae3f8201879eae530b6216e15
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-22 00:18:01 +0100

    23 questions

--- src/index.js
+++ src/index.js
@@ -1,36 +1,54 @@
-Remember the acronym UPS check
+// Remember the acronym UPS check
 
-Step One (Understand). We have to understand the problem.
-1. What is the unknown?
+// Step One (Understand). We have to understand the problem.
+// 1. What is the unknown?
 
+// the total sum of all the Nodes
 
-2. What are the data inputs?
-3. Can you restate the problem in your own words?
-4. Can the unknown be determined from the data inputs?
-5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? 
-6. How should I label important pieces of data input that are a part of the problem?
-7. Draw a figure. Introduce suitable notation.
-8. Separate the various parts of the condition. Can you write them down?
+// 2. What are the data inputs?
+// head
 
-Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
-9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem? 
-10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
-11.    Should you introduce some auxiliary element in order to make its use possible? 
-12. Start with a simple example. Could you restate the problem? Could you restate it still differently? 
-13. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs? 
-14. Progress to more complex examples. What is your updated plan here to find a solution?
-15. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
+// 3. Can you restate the problem in your own words?
+// traverse the linked list and return the total
 
+// 4. Can the unknown be determined from the data inputs?
+// yes
 
-Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
-16.  Can you see clearly that the step is correct?
+// 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory?
 
+// sufficient
 
-IV. 	Step Four (Check). Examine the solution obtained and refactor. 
-17.  Can you check your result? 
-18.  Can you check the argument? 
-19.  Can you derive the result differently? 
-20.  Can you see it at a glance? 
-21.  Can you make the code DRYer and refactor? 
-22.  Can you improve the performance? 
-23.   How have other people solved this problem?
+// 6. How should I label important pieces of data input that are a part of the problem?
+
+// head
+// 7. Draw a figure. Introduce suitable notation.
+
+// in notepad
+// 8. Separate the various parts of the condition. Can you write them down?
+
+// Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found.
+// 9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem?
+// link list traversal
+
+// 10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method?
+// count and while loop
+
+// 11.    Should you introduce some auxiliary element in order to make its use possible?
+// 12. Start with a simple example. Could you restate the problem? Could you restate it still differently?
+// 13. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs?
+
+// 14. Progress to more complex examples. What is your updated plan here to find a solution?
+// 15. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
+
+// Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
+// 16.  Can you see clearly that the step is correct?
+
+// see pseudocode
+// IV. 	Step Four (Check). Examine the solution obtained and refactor.
+// 17.  Can you check your result?
+// 18.  Can you check the argument?
+// 19.  Can you derive the result differently?
+// 20.  Can you see it at a glance?
+// 21.  Can you make the code DRYer and refactor?
+// 22.  Can you improve the performance?
+// 23.   How have other people solved this problem?
